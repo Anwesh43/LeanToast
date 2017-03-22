7bf45c47434dce5c24c2ac6babdb9f6294361c8e
Anwesh43 <anweshthecool0@gmail.com>
2017-03-22 16:52:59 +0530

    LeanToast Demo in app

---
+++ app/src/main/java/com/anwesome/ui/leantoast/LeanToast.java
@@ -0,0 +1,26 @@
+package com.anwesome.ui.leantoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LeanToast extends AppCompatActivity {
+    private int time = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lean_toast);
+        Button showToast = (Button)findViewById(R.id.showtoast);
+        final TextView numberOfToasts = (TextView)findViewById(R.id.number_of_texts);
+        showToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                time++;
+                com.anwesome.games.leantoastui.LeanToast.show(LeanToast.this,"Showing LeanToast"+(time), com.anwesome.games.leantoastui.LeanToast.LENGTH_LONG);
+                numberOfToasts.setText("number of toasts "+time);
+            }
+        });
+    }
+}
