766b60d06a68da42296ac07adc831567556267cb
Anwesh43 <anweshthecool0@gmail.com>
2017-03-22 13:41:28 +0530

    Adding AnimationAdapter

---
+++ leantoastui/src/main/java/com/anwesome/games/leantoastui/AnimationAdapter.java
@@ -0,0 +1,25 @@
+package com.anwesome.games.leantoastui;
+
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+
+/**
+ * Created by anweshmishra on 22/03/17.
+ */
+public class AnimationAdapter implements ValueAnimator.AnimatorUpdateListener,Animator.AnimatorListener {
+    public void onAnimationEnd(Animator animator) {
+
+    }
+    public void onAnimationStart(Animator animator) {
+
+    }
+    public void onAnimationRepeat(Animator animator) {
+
+    }
+    public void onAnimationCancel(Animator animator) {
+
+    }
+    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+
+    }
+}
